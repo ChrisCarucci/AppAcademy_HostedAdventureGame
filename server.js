14a686a3c08188607b74894dc73ca45b174664fe
ChrisCarucci <Chris.w.carucci@gmail.com>
2022-11-24 21:27:42 -0330

    Adventure Game Hosted Via Browser

---
+++ server.js
@@ -0,0 +1,151 @@
+const http = require('http');
+const fs = require('fs');
+
+const { Player } = require('./game/class/player');
+const { World } = require('./game/class/world');
+
+const worldData = require('./game/data/basic-world-data');
+
+let player;
+let world = new World();
+world.loadWorld(worldData);
+
+const server = http.createServer((req, res) => {
+
+  /* ============== ASSEMBLE THE REQUEST BODY AS A STRING =============== */
+  let reqBody = '';
+  req.on('data', (data) => {
+    reqBody += data;
+  });
+
+  req.on('end', () => { // After the assembly of the request body is finished
+    /* ==================== PARSE THE REQUEST BODY ====================== */
+    if (reqBody) {
+      req.body = reqBody
+        .split("&")
+        .map((keyValuePair) => keyValuePair.split("="))
+        .map(([key, value]) => [key, value.replace(/\+/g, " ")])
+        .map(([key, value]) => [key, decodeURIComponent(value)])
+        .reduce((acc, [key, value]) => {
+          acc[key] = value;
+          return acc;
+        }, {});
+    }
+
+    /* ======================== ROUTE HANDLERS ========================== */
+    // Phase 1: GET /
+    if (req.method === 'GET' && req.url === '/') {
+      const htmlTemplate = fs.readFileSync('./views/new-player.html', 'utf-8');
+      const htmlPage = htmlTemplate.replace(/#{availableRooms}/g, world.availableRoomsToString());
+      console.log(player);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      return res.end(htmlPage);
+    }
+
+    // Phase 2: POST /player
+    if (req.method === 'POST' && req.url === '/player') {
+      const { name, roomId } = req.body;
+      player = new Player(name, world.rooms[roomId]);
+      res.statusCode = 302;
+      res.setHeader('Location', `/rooms/${roomId}`);
+      return res.end();
+    }
+
+    if (!player) {
+      res.setHeader('location', '/');
+      res.statusCode = 302;
+      return res.end();
+    }
+
+    // Phase 3: GET /rooms/:roomId
+    if (req.method === "GET" && req.url.startsWith('/rooms/')) {
+      const urlParts = req.url.split('/');
+      if (urlParts.length === 3) {
+        const worldRoom = world.rooms[urlParts[2]];
+
+        if (worldRoom !== player.currentRoom) {
+          for (const room in world.rooms) {
+            if (world.rooms[room].name === player.currentRoom.name) {
+              res.setHeader('location', `/rooms/${room}`);
+              res.statusCode = 302;
+              return res.end();
+            }
+          }
+        }
+
+        const htmlTemplate = fs.readFileSync('./views/room.html', 'utf-8');
+        const htmlPage = htmlTemplate
+          .replace(/#{roomName}/g, player.currentRoom.name)
+          .replace(/#{roomItems}/g, player.currentRoom.itemsToString())
+          .replace(/#{exits}/g, player.currentRoom.exitsToString())
+          .replace(/#{inventory}/g, player.inventoryToString());
+        console.log(player);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        return res.end(htmlPage);
+      }
+    }
+
+    // Phase 4: GET /rooms/:roomId/:direction
+    if (req.method === 'GET' && req.url.startsWith(`/rooms/${player.currentRoom.id}/`)) {
+      const reqPath = req.url.split('/');
+      const reqExit = reqPath[3][0];
+      player.move(reqExit);
+
+      const htmlTemplate = fs.readFileSync('./views/room.html', 'utf-8');
+      const htmlPage = htmlTemplate
+        .replace(/#{roomName}/g, player.currentRoom.name)
+        .replace(/#{roomItems}/g, player.currentRoom.itemsToString())
+        .replace(/#{exits}/g, player.currentRoom.exitsToString())
+        .replace(/#{inventory}/g, player.inventoryToString());
+      console.log(player);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      return res.end(htmlPage);
+    }
+
+
+    // Phase 5: POST /items/:itemId/:action
+
+    if (req.method === 'POST' && req.url.startsWith('/items/')) {
+      let action = req.url.split('/')[3];
+      let item = req.url.split('/')[2];
+
+      switch(action) {
+        case 'take':
+          player.takeItem(item)
+          break;
+        case 'drop':
+          player.dropItem(item)
+          break;
+        case 'eat':
+          player.eatItem(item)
+          break;
+      }
+
+      res.statusCode = 302;
+      res.setHeader('Location', `/rooms/${player.currentRoom.id}`);
+      return res.end()
+
+    }
+
+
+    // Phase 6: Redirect if no matching route handlers
+    let currRoom;
+    for (let room in world.rooms) {
+      if (world.rooms[room].name === player.currentRoom.name) {
+        currRoom = room;
+      }
+    }
+    res.statusCode = 302;
+    res.setHeader('Location', `/rooms/${currRoom}`)
+    console.log("No matching handler found..")
+    return res.end()
+
+  })
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log('Server is listening on port', port));
